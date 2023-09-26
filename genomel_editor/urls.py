8025002aacefe887e6d421407a2540435258ee16
Jan Hajic Jr <hajicj@ufal.mff.cuni.cz>
2023-09-26 19:53:40 +0200

    Changed app URL for multi-app deployment.

--- genomel_editor/urls.py
+++ genomel_editor/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('GenomelEditor.urls'), name='GenomelEditor'),
+    path("genomel_editor/", include('GenomelEditor.urls'), name='GenomelEditor'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
